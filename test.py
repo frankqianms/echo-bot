5e8f861556b574172d2b90d262a5ab6ab9a045d7
frankqianms <frankqian@microsoft.com>
2023-10-19 09:46:39 +0800

    repro error

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+from typing import Dict, List, Union
 from botbuilder.core import (
     BotFrameworkAdapterSettings,
     TurnContext,
@@ -5,92 +6,64 @@ from botbuilder.core import (
 )
 from botbuilder.core.integration import aiohttp_error_middleware
 from botbuilder.schema import Activity, ActivityTypes
+from botframework.connector.auth import (
+    ClaimsIdentity,
+    CredentialProvider,
+    ChannelProvider,
+    AuthenticationConfiguration,
+    JwtTokenValidation,
+    ChannelValidation,
+    EnterpriseChannelValidation,
+    AuthenticationConstants,
+    JwtTokenExtractor,
+)
 
-async def get_claims(
-    auth_header: str,
-    credentials: CredentialProvider,
-    channel_service_or_provider: Union[str, ChannelProvider],
-    channel_id: str,
-    service_url: str = None,
-    auth_configuration: AuthenticationConfiguration = None,
-) -> ClaimsIdentity:
-    if SkillValidation.is_skill_token(auth_header):
-        return await SkillValidation.authenticate_channel_token(
-            auth_header,
-            credentials,
-            channel_service_or_provider,
-            channel_id,
-            auth_configuration,
-        )
+async def validate_identity(
+        identity: ClaimsIdentity, credentials: CredentialProvider
+):
+    if not identity:
+        # No valid identity. Not Authorized.
+        raise PermissionError("Unauthorized. No valid identity.")
 
-    if EmulatorValidation.is_token_from_emulator(auth_header):
-        return await EmulatorValidation.authenticate_emulator_token(
-            auth_header, credentials, channel_service_or_provider, channel_id
-        )
+    if not identity.is_authenticated:
+        # The token is in some way invalid. Not Authorized.
+        raise PermissionError("Unauthorized. Is not authenticated")
 
-    is_public = (
-        not channel_service_or_provider
-        or isinstance(channel_service_or_provider, ChannelProvider)
-        and channel_service_or_provider.is_public_azure()
-    )
-    is_gov = (
-        isinstance(channel_service_or_provider, ChannelProvider)
-        and channel_service_or_provider.is_public_azure()
-        or isinstance(channel_service_or_provider, str)
-        and JwtTokenValidation.is_government(channel_service_or_provider)
-    )
+    # Now check that the AppID in the claimset matches
+    # what we're looking for. Note that in a multi-tenant bot, this value
+    # comes from developer code that may be reaching out to a service, hence the
+    # Async validation.
 
-    # If the channel is Public Azure
-    if is_public:
-        if service_url:
-            return await ChannelValidation.authenticate_channel_token_with_service_url(
-                auth_header,
-                credentials,
-                service_url,
-                channel_id,
-                auth_configuration,
-            )
+    # Look for the "aud" claim, but only if issued from the Bot Framework
+    if (
+        identity.get_claim_value(AuthenticationConstants.ISSUER_CLAIM)
+        != AuthenticationConstants.TO_BOT_FROM_CHANNEL_TOKEN_ISSUER
+    ):
+        # The relevant Audience Claim MUST be present. Not Authorized.
+        raise PermissionError("Unauthorized. Audience Claim MUST be present.")
 
-        return await ChannelValidation.authenticate_channel_token(
-            auth_header, credentials, channel_id, auth_configuration
-        )
+    # The AppId from the claim in the token must match the AppId specified by the developer.
+    # Note that the Bot Framework uses the Audience claim ("aud") to pass the AppID.
+    aud_claim = identity.get_claim_value(AuthenticationConstants.AUDIENCE_CLAIM)
+    return aud_claim
 
-    if is_gov:
-        if service_url:
-            return await GovernmentChannelValidation.authenticate_channel_token_with_service_url(
-                auth_header,
-                credentials,
-                service_url,
-                channel_id,
-                auth_configuration,
-            )
-
-        return await GovernmentChannelValidation.authenticate_channel_token(
-            auth_header, credentials, channel_id, auth_configuration
-        )
-
-    # Otherwise use Enterprise Channel Validation
-    if service_url:
-        return await EnterpriseChannelValidation.authenticate_channel_token_with_service_url(
-            auth_header,
-            credentials,
-            service_url,
-            channel_id,
-            channel_service_or_provider,
-            auth_configuration,
-        )
-
-    return await EnterpriseChannelValidation.authenticate_channel_token(
-        auth_header,
-        credentials,
-        channel_id,
-        channel_service_or_provider,
-        auth_configuration,
-    )
-
-async def decode_auth_header(adapter: BotFrameworkAdapter, activity, auth_header, app_id):
+async def decode_auth_header(adapter: BotFrameworkAdapter, activity: Activity, auth_header, app_id):
     credentials = adapter._credential_provider
     channel_provider = await adapter.settings.channel_provider.get_channel_service()
     auth_configuration = adapter.settings.auth_configuration
-    claims_identity = await adapter._authenticate_request(activity, auth_header)
-    return claims_identity.claims
+    metadata_endpoint = (
+        ChannelValidation.open_id_metadata_endpoint
+        if ChannelValidation.open_id_metadata_endpoint
+        else AuthenticationConstants.TO_BOT_FROM_CHANNEL_OPEN_ID_METADATA_URL
+    )
+
+    token_extractor = JwtTokenExtractor(
+        ChannelValidation.TO_BOT_FROM_CHANNEL_TOKEN_VALIDATION_PARAMETERS,
+        metadata_endpoint,
+        AuthenticationConstants.ALLOWED_SIGNING_ALGORITHMS,
+    )
+    identity = await token_extractor.get_identity_from_auth_header(
+        auth_header, activity.channel_id, auth_configuration.required_endorsements
+    )
+    # claims_identity = await get_claims(auth_header, credentials, channel_provider, activity.channel_id, activity.service_url, auth_configuration)
+    return await validate_identity(identity, credentials)
